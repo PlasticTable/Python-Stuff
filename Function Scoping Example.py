7a7b69aaf7e7140df585517842ee7865e6e7b197
Diana Tsai (天云) <didit7541@gmail.com>
2017-04-13 13:15:06 -0400

    Create Function Scoping Example.py

---
+++ Function Scoping Example.py
@@ -0,0 +1,13 @@
+lis1 = [1,2,3,4]
+
+def random(lis1):
+  lis2 = []
+  for i in lis1:
+    lis2.append(i)
+  return lis2
+
+print random(lis1)
+print random([9,4,3,2])
+
+# Very ugly and confusing code
+# Was wondering why it worked
